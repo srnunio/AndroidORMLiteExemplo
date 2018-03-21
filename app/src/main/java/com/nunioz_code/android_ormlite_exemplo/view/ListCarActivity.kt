7f47588d6fe5f374a718d492ac8799bbe5a07c69
Unknown <edvaldonuniomartins@gmail.com>
2018-03-21 13:16:55 +0100

    add metodo

--- app/src/main/java/com/nunioz_code/android_ormlite_exemplo/view/ListCarActivity.kt
+++ app/src/main/java/com/nunioz_code/android_ormlite_exemplo/view/ListCarActivity.kt
@@ -26,22 +26,24 @@ class ListCarActivity : AppCompatActivity() {
         recycler.setHasFixedSize(false)
         recycler.layoutManager = LinearLayoutManager(this)
         recycler.adapter = carAdpter
-        carAdpter!!.addItens(dao.queryForAll())
-
         novoCar.setOnClickListener({
             startActivityForResult(
                     Intent(this, CreateCarActivity::class.java), 0
             )
         })
+        updateList()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+       updateList()
+    }
+
+    fun updateList(){
         val list = dao.queryForAll()
         Collections.reverse(list)
         carAdpter!!.addItens(list)
     }
-
     override fun onDestroy() {
         dbHelper.close()
         super.onDestroy()
